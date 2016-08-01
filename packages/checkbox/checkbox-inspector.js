70875d1dfd5ba04e5fd7c520ceb8e805fcc6c7ff
zilongshanren <guanghui8827@126.com>
2016-08-01 10:39:42 +0800

    add eslintrc

--- packages/checkbox/checkbox-inspector.js
+++ packages/checkbox/checkbox-inspector.js
@@ -4,22 +4,11 @@ Vue.component('cc-button', {
   template: `
     <ui-prop v-prop="target.target"></ui-prop>
     <ui-prop v-prop="target.interactable"></ui-prop>
-    <ui-prop v-prop="target.transition"></ui-prop>
 
-    <div v-if="target.transition.value === 1">
-      <ui-prop indent=1 v-prop="target.normalColor"></ui-prop>
-      <ui-prop indent=1 v-prop="target.pressedColor"></ui-prop>
-      <ui-prop indent=1 v-prop="target.hoverColor"></ui-prop>
-      <ui-prop indent=1 v-prop="target.disabledColor"></ui-prop>
-      <ui-prop indent=1 v-prop="target.duration"></ui-prop>
-    </div>
-
-    <div v-if="target.transition.value === 2">
-      <ui-prop indent=1 v-prop="target.normalSprite"></ui-prop>
-      <ui-prop indent=1 v-prop="target.pressedSprite"></ui-prop>
-      <ui-prop indent=1 v-prop="target.hoverSprite"></ui-prop>
-      <ui-prop indent=1 v-prop="target.disabledSprite"></ui-prop>
-    </div>
+    <ui-prop indent=1 v-prop="target.normalSprite"></ui-prop>
+    <ui-prop indent=1 v-prop="target.pressedSprite"></ui-prop>
+    <ui-prop indent=1 v-prop="target.hoverSprite"></ui-prop>
+    <ui-prop indent=1 v-prop="target.disabledSprite"></ui-prop>
 
     <cc-array-prop :target.sync="target.clickEvents"></cc-array-prop>
   `,
