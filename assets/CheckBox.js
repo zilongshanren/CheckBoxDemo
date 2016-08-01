0ff3462bf725573a812af9d30a98de6fbb59fb71
zilongshanren <guanghui8827@126.com>
2016-08-01 12:05:27 +0800

    refine checkbox inspector

--- assets/CheckBox.js
+++ assets/CheckBox.js
@@ -6,11 +6,79 @@ cc.Class({
     },
 
     properties: {
+        _spriteFrames: [],
+
+
+        isChecked: {
+            default: true,
+            notify: function() {
+                this._updateSprites();
+            }
+        },
+
+        inActiveNormalSprite: {
+            default: null,
+            type: cc.SpriteFrame,
+            notify: function() {
+                this._updateSprites();
+            }
+        },
+
+        activeNormalSprite: {
+            default: null,
+            type: cc.SpriteFrame,
+            notify: function () {
+                this._updateSprites();
+            }
+        },
+
+        activePressedSprite: {
+            default: null,
+            type: cc.SpriteFrame,
+            notify: function () {
+                this._updateSprites();
+            }
+        },
+
+        inActivePressedSprite: {
+            default: null,
+            type: cc.SpriteFrame,
+            notify: function () {
+                this._updateSprites();
+            }
+        },
+
+        activeDisabledSprite: {
+            default: null,
+            type: cc.SpriteFrame,
+            notify: function () {
+                this._updateSprites();
+            }
+        },
+
+        inActiveDisabledSprite: {
+            default: null,
+            type: cc.SpriteFrame,
+            notify: function () {
+                this._updateSprites();
+            }
+        }
+    },
+
+    _updateSprites: function () {
+        if(this.isChecked) {
+            this.normalSprite = this.activeNormalSprite;
+            this.pressedSprite = this.activePressedSprite;
+            this.disabledSprite = this.activeDisabledSprite;
+        } else {
+            this.normalSprite = this.inActiveNormalSprite;
+            this.normalSprite = this.inActivePressedSprite;
+            this.disabledSprite = this.inActiveDisabledSprite;
+        }
     },
 
     // use this for initialization
     onLoad: function () {
-
     },
 
 });
--- packages/checkbox/checkbox-inspector.js
+++ packages/checkbox/checkbox-inspector.js
@@ -5,10 +5,15 @@ Vue.component('cc-button', {
     <ui-prop v-prop="target.target"></ui-prop>
     <ui-prop v-prop="target.interactable"></ui-prop>
 
-    <ui-prop indent=1 v-prop="target.normalSprite"></ui-prop>
-    <ui-prop indent=1 v-prop="target.pressedSprite"></ui-prop>
-    <ui-prop indent=1 v-prop="target.hoverSprite"></ui-prop>
-    <ui-prop indent=1 v-prop="target.disabledSprite"></ui-prop>
+    <ui-prop v-prop="target.isChecked"></ui-prop>
+
+    <ui-prop v-prop="target.activeNormalSprite"></ui-prop>
+    <ui-prop v-prop="target.activePressedSprite"></ui-prop>
+    <ui-prop v-prop="target.activeDisabledSprite"></ui-prop>
+
+    <ui-prop v-prop="target.inActiveNormalSprite"></ui-prop>
+    <ui-prop v-prop="target.inActivePressedSprite"></ui-prop>
+    <ui-prop v-prop="target.inActiveDisabledSprite"></ui-prop>
 
     <cc-array-prop :target.sync="target.clickEvents"></cc-array-prop>
   `,
